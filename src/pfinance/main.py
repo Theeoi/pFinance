60f7628ca780646271ed545284d80e5d12e30156
Theodor Blom <me@theodorblom.com>
2022-10-19 14:15:08 +0200

    Implemented multitable database

--- src/pfinance/main.py
+++ src/pfinance/main.py
@@ -42,22 +42,35 @@ class Database:
         """
         return os.path.exists(self.path)
 
-    def read_database(self) -> pd.DataFrame | None:
+    def read_database(self) -> dict[str, pd.DataFrame | None]:
         """
-        Returns database entries as a DataFrame.
+        Returns database entries in {table} as a DataFrame.
         If database is empty or does not exist return None.
         """
-        try:
-            return pd.read_sql("""
-                    SELECT * FROM ledger
-                    """, self.conn, index_col=['Transaction date', 'Category'],
-                               parse_dates=['Transaction date'])
-        except KeyError:
-            return None
-        except pd.errors.DatabaseError:
-            return None
+        df_dict = {}
 
-    def load_to_database(self, xl_path: str) -> None:
+        query_tables = """
+        SELECT name FROM sqlite_master WHERE type='table';
+        """
+        self.curr.execute(query_tables)
+        tables = [v[0] for v in self.curr.fetchall() if v[0] !=
+                  "sqlite_sequence"]
+        for table in tables:
+            try:
+                df_dict[table] = pd.read_sql(f"""SELECT * FROM {table}""",
+                                             self.conn,
+                                             index_col=[
+                                                 'Transaction date', 'Category'
+                                             ],
+                                             parse_dates=['Transaction date'])
+            except KeyError:
+                df_dict[table] = None
+            except pd.errors.DatabaseError:
+                df_dict[table] = None
+
+        return df_dict
+
+    def load_to_database(self, table: str, xl_path: str) -> None:
         """
         Read excel-file into database table.
         Appends new entries through replacing old matching ones.
@@ -68,17 +81,17 @@ class Database:
         # Set category for each expense here!
         input_data = input_data.set_index(['Transaction date', 'Category'])
 
-        current_db: pd.DataFrame | None = self.read_database()
-        if current_db is None:
+        current_db: dict[str, pd.DataFrame | None] = self.read_database()
+        if table not in current_db or current_db[table] is None:
             agg_data: pd.DataFrame = input_data
         else:
-            agg_data = current_db.combine_first(input_data)
+            agg_data = current_db[table].combine_first(input_data)
             agg_data.drop_duplicates(inplace=True)
 
         agg_data.sort_index(level='Transaction date', inplace=True,
                             ascending=False)
 
-        agg_data.to_sql(name="ledger", con=self.conn, schema=DB_SCHEMA,
+        agg_data.to_sql(name=table, con=self.conn, schema=DB_SCHEMA,
                         if_exists='replace', index=True,
                         index_label=['Transaction date', 'Category'])
         self.conn.commit()
@@ -94,9 +107,12 @@ def get_cliargs() -> dict:
     parser.add_argument("-s", "--show", action='store_true',
                         help="Shows the current data in database.")
 
-    parser.add_argument("-l", "--load", type=str, nargs=1, metavar="file_path",
+    parser.add_argument("-l", "--load", type=str, nargs=2,
+                        metavar=("table_name", "file_path"),
                         default=None,
-                        help="Loads the specified .ods file to sql database.")
+                        help="""
+                        Loads the specified .ods file to specified table in sql database.
+                        """)
 
     return vars(parser.parse_args())
 
@@ -113,7 +129,8 @@ def main():
         print(f"Current database:\n{db.read_database()}")
 
     if args['load'] is not None:
-        file_path = args['load'][0]
+        table_name = args['load'][0]
+        file_path = args['load'][1]
         if not os.path.exists(file_path):
             raise ValueError(f"""
                     ValueError: Invalid file path/name. Path {file_path}
@@ -124,7 +141,7 @@ def main():
                     ValueError: Invalid file format. {file_path} must be a
                     .ods file.
                     """)
-        db.load_to_database(file_path)
+        db.load_to_database(table_name, file_path)
 
     # Closing all active databases
     for db_instance in Database.instances:
